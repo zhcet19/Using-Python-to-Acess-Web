e55c347824a7aa115d86d339036a42ecbe0b5cf8
Faiz alam <55043876+zhcet19@users.noreply.github.com>
2020-06-10 17:50:26 +0530

    To access a mth link on nth webpage

---
+++ Assignment4.2.py
@@ -0,0 +1,26 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+     # Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input("Enter Url: ")
+count = int(input("Enter count: "))
+position = int(input("Enter position: "))
+
+for i in range(count):
+         html = urlopen(url, context=ctx).read()
+         soup = BeautifulSoup(html, "html.parser")
+
+         tags = soup('a')
+         list1 = []
+
+         for tag in tags:
+             x = tag.get('href', None)
+             list1.append(x)
+
+         print(list1[position - 1])
+         url=list1[position-1]
